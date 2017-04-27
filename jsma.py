43475ae2b49c7c95a9bbfe8e3c61556bcda7573a
Anubhav Ashok <anubhava@andrew.cmu.edu>
2017-04-26 21:05:07 -0400

    Updated JSMA code

--- jsma.py
+++ jsma.py
@@ -6,61 +6,44 @@ from cleverhans.utils import cnn_model
 import keras
 from keras import backend
 from keras.utils import np_utils
+from keras.applications import vgg16
 from glob import glob
 import numpy as np
+import cv2
+import os
 
-
-# Loads a single image from the dataset and computes jsma perturbation
-
-# Path to the directory of the images
-# NOTE: need to move images out of all folders into the main directory for this to work
-dataset_dir = "./data/dogs/*.{}".format('jpg')
 
 FLAGS = flags.FLAGS
 FLAGS.nb_classes = 10
-# Reduce gamma for faster but lower quality processing 
+
 _gamma = 0.01
+target = 2
 
-# Read images in tensorflow symbolically
-filename_queue = tf.train.string_input_producer(
-    tf.train.match_filenames_once(dataset_dir))
+imgs = glob('./data/dogs/*.jpg')
 
-image_reader = tf.WholeFileReader()
-
-_, image_file = image_reader.read(filename_queue)
-
-image = tf.image.decode_jpeg(image_file)
-image = tf.image.resize_image_with_crop_or_pad(image, 224, 224)
-
-
-# Start a new session to show example output.
-with tf.Session() as sess:
-    keras.backend.set_session(sess)
-    x = tf.placeholder(tf.float32, shape=(None, 224, 224, 3))
-    model = cnn_model(img_rows=224, img_cols=224, channels=3)
-    predictions = model(x)
-    grads = jacobian_graph(predictions, x, 10)
-    sess.run(tf.global_variables_initializer())
-    
-    # Coordinate the loading of image files.
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(coord=coord)
-    
-    image_tensor = sess.run([image])[0]
-    print(image_tensor.shape)
-    image_tensor = image_tensor[np.newaxis,:]
-    
-    # Target class
-    target = 2
-    adv_x, res, percent_perturb = jsma(sess, x, predictions, grads,
+sess = tf.InteractiveSession()
+keras.backend.set_session(sess)
+raw_img = tf.placeholder(tf.float32, shape=(None, None, None, 3))
+img = tf.image.resize_image_with_crop_or_pad(raw_img, 224, 224)
+model = vgg16.VGG16(input_tensor=img, weights='imagenet')
+predictions = model(img)
+grads = jacobian_graph(predictions, img, 10) 
+'''
+adv_x, res, percent_perturb = jsma(sess, img, predictions, grads,
                                                image_tensor,
                                                target, theta=1, gamma=_gamma,
                                                increase=True, back='tf',
                                                clip_min=0, clip_max=1)
+'''
+
+
+for i in imgs:
+    data = cv2.imread(i)
+    data = sess.run(img, feed_dict={raw_img:[data]}) 
+    adv_x, res, percent_perturb = jsma(sess, img, predictions, grads,
+                                               data,
+                                               target, theta=1, gamma=_gamma,
+                                               increase=True, back='tf',
+                                               clip_min=0, clip_max=1)
+    #adversarial = sess.run(adv_x, feed_dict={raw_img: [data]})
     print(adv_x)
-    
-    # Finish off the filename queue coordinator.
-    coord.request_stop()
-    coord.join(threads)
-
-
