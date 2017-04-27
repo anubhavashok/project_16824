7fd4b729c67a6060e54dd8490b57fc55a7b33703
Anubhav Ashok <anubhava@andrew.cmu.edu>
2017-04-26 22:06:06 -0400

    Added fc layers to model

--- jsma.py
+++ jsma.py
@@ -7,6 +7,8 @@ import keras
 from keras import backend
 from keras.utils import np_utils
 from keras.applications import vgg16
+from keras.layers import Input, Flatten, Dense
+from keras.models import Model
 from glob import glob
 import numpy as np
 import cv2
@@ -16,7 +18,7 @@ import os
 FLAGS = flags.FLAGS
 FLAGS.nb_classes = 10
 
-_gamma = 0.01
+_gamma = 0.1
 target = 2
 
 imgs = glob('./data/dogs/*.jpg')
@@ -25,17 +27,19 @@ sess = tf.InteractiveSession()
 keras.backend.set_session(sess)
 raw_img = tf.placeholder(tf.float32, shape=(None, None, None, 3))
 img = tf.image.resize_image_with_crop_or_pad(raw_img, 224, 224)
-model = vgg16.VGG16(input_tensor=img, weights='imagenet')
+#input = Input(shape=(None, 224, 224, 3), name='image_input')
+vgg_16_conv = vgg16.VGG16(input_tensor=img, weights='imagenet', include_top=False)
+x = Flatten(name='flatten')(vgg_16_conv.output)
+x = Dense(4096, activation='relu', name='fc1')(x)
+x = Dense(4096, activation='relu', name='fc2')(x)
+x = Dense(10, activation='softmax', name='predictions')(x)
+model = Model(input=vgg_16_conv.input, output=x)
+model.summary()
 predictions = model(img)
 grads = jacobian_graph(predictions, img, 10) 
-'''
-adv_x, res, percent_perturb = jsma(sess, img, predictions, grads,
-                                               image_tensor,
-                                               target, theta=1, gamma=_gamma,
-                                               increase=True, back='tf',
-                                               clip_min=0, clip_max=1)
-'''
+sess.run(tf.global_variables_initializer())
 
+print(sess.run(predictions, feed_dict={raw_img:[cv2.imread(imgs[0])]}))
 
 for i in imgs:
     data = cv2.imread(i)
