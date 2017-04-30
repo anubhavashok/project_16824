0e8e15efb9f5c3b648314a68d00dd874b5bec41e
Anubhav Ashok <anubhava@compute-0-37.local>
2017-04-30 05:43:59 -0400

    Updated fgsm code to save in the correct format

--- fgsm.py
+++ fgsm.py
@@ -7,31 +7,29 @@ from keras import backend
 from keras.utils import np_utils
 from keras.applications import vgg16
 from glob import glob
+from imagenet_utils import preprocess_input
 import numpy as np
 import cv2
 import os
 
-imgs = glob('../dogs/*.jpg')
+imgs = glob('../dogs_box/*.jpg')
 _eps = 0.1
 sess = tf.InteractiveSession()
 keras.backend.set_session(sess)
 x = tf.placeholder(tf.float32, shape=(None, 224, 224, 3))
 raw_img = tf.placeholder(tf.float32, shape=(None, None, 3))
 #img = tf.image.resize_image_with_crop_or_pad(raw_img, 224, 224)
-img = tf.resize_images(raw_img, [224, 224], alignCorners=True)
-#model = cnn_model(img_rows=224, img_cols=224, channels=3)
+img = tf.image.resize_images(raw_img, [224, 224], align_corners=True)
 model = vgg16.VGG16(input_tensor=tf.expand_dims(img, 0), weights='imagenet')
-#predictions = model(img)
-#adv_x, signed_grad = fgsm(img, predictions, eps=_eps)
+predictions = model(tf.expand_dims(img, 0))
+adv_x, signed_grad = fgsm(img, predictions, eps=_eps)
 sess.run(tf.global_variables_initializer())
 sess.run(tf.local_variables_initializer())
 
 for i in imgs:
     data = cv2.imread(i)
-    #adv = sess.run(adv_x, {raw_img: [data], keras.backend.learning_phase(): 0})
-    #grad = sess.run(signed_grad, {raw_img: [data], keras.backend.learning_phase(): 0})
-    ri = sess.run(img, {raw_img: data})
-    #adv_path = './data/dogs_grad_{}/'.format(_eps) + os.path.basename(i).split('.')[0] + '.jpg'
-    path = './data/dogs_cropped/' + os.path.basename(i)
-    #cv2.imwrite(adv_path, adv[0])
-    cv2.imwrite(path, ri[0])
+    data = preprocess_input(np.expand_dims(data, 0))[0]
+    grad = sess.run(signed_grad, {raw_img: data, keras.backend.learning_phase(): 0})
+    adv_path = './data/dogs_box_grad/'  + os.path.basename(i).split('.')[0] + '.jpg'
+    print(adv_path)
+    cv2.imwrite(adv_path, grad[0]+1)
